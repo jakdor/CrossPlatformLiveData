00e3a98a7e289a82d5ef44f5fb5185b39c122182
Jakub Dorda <j.dorda@tmssoft.pl>
2019-09-02 13:24:09 +0200

    add CrossPlatformLiveData.Android subproject

---
+++ CrossPlatformLiveData.Android/LiveDataSupportFragment.cs
@@ -0,0 +1,52 @@
+﻿using Android.Support.V4.App;
+
+namespace CrossPlatformLiveData.Android
+{
+    public class LiveDataSupportFragment : Fragment
+    {
+        protected readonly ILifecycleManager LifecycleManager = new LifecycleManager();
+
+        public override void OnHiddenChanged(bool hidden)
+        {
+            base.OnHiddenChanged(hidden);
+            if (hidden)
+            {
+                LifecycleManager?.OnPause();
+            }
+            else
+            {
+                LifecycleManager?.OnResume();
+            }
+        }
+
+        public override void OnResume()
+        {
+            base.OnResume();
+            if (!IsHidden)
+            {
+                LifecycleManager?.OnResume();
+            }
+        }
+
+        public override void OnPause()
+        {
+            base.OnPause();
+            if (!IsHidden)
+            {
+                LifecycleManager?.OnPause();
+            }
+        }
+
+        public override void OnDestroyView()
+        {
+            base.OnDestroyView();
+            LifecycleManager?.OnDestroyView();
+        }
+
+        public override void OnDestroy()
+        {
+            base.OnDestroy();
+            LifecycleManager?.Dispose();
+        }
+    }
+}
