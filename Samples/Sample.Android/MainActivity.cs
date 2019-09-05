82495347b7c3c18b2bb664666f8e0ea84efee35c
Jakub Dorda <j.dorda@tmssoft.pl>
2019-09-05 16:31:51 +0200

    modify dependencies, add nuspec configs for Android/iOS sub libraries

--- Samples/Sample.Android/MainActivity.cs
+++ Samples/Sample.Android/MainActivity.cs
@@ -1,11 +1,12 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Support.V7.App;
+using CrossPlatformLiveData.Android;
 
 namespace Sample.Android
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : LiveDataAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
