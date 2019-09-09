61b7a8fffa7b58bf98378ed6c0af0c152c5b2429
Jakub Dorda <j.dorda@tmssoft.pl>
2019-09-09 11:33:51 +0200

    add RxWrapper

---
+++ CrossPlatformLiveData/RxWrapper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CrossPlatformLiveData
+{
+    /// <summary>
+    /// Rx event wrapper template - optional model class bundling data, status and exception in single response.
+    /// </summary>
+    public class RxWrapper<T>
+    {
+        public Enum Status { get; set; }
+        public T Data { get; set; }
+        public Exception Exception { get; set; }
+
+        public RxWrapper(T data)
+        {
+            Status = RxStatus.Ok;
+            Data = data;
+        }
+
+        public RxWrapper(Enum status)
+        {
+            Status = status;
+        }
+
+        public RxWrapper(Enum status, T data)
+        {
+            Status = status;
+            Data = data;
+        }
+
+        public RxWrapper(Exception e)
+        {
+            Status = RxStatus.Error;
+            Exception = e;
+        }
+
+        public static RxWrapper<T> Ok(T data) => new RxWrapper<T>(data);
+        public static RxWrapper<T> Pending() => new RxWrapper<T>(RxStatus.Pending);
+        public static RxWrapper<T> Error(Exception e) => new RxWrapper<T>(e);
+    }
+
+    public enum RxStatus
+    {
+        Ok, Pending, Error
+    }
+}
