84d11caa6eafa7b551c77931203a9808b325f03e
Jakub Dorda <j.dorda@tmssoft.pl>
2019-09-10 10:10:05 +0200

    config Sample.iOS

---
+++ Samples/Sample.iOS/SampleViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Blank
+{
+    public partial class SampleViewController : UIViewController
+    {
+        public SampleViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
